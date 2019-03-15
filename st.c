dff23e1efcf095e2c46b5c67e5670a5ff666948b
Hiltjo Posthuma <hiltjo@codemadness.org>
2019-03-15 14:42:50 +0100

    minor code-style, initialize var at the top of function

--- st.c
+++ st.c
@@ -1854,7 +1854,7 @@ csireset(void)
 void
 strhandle(void)
 {
-	char *p = NULL;
+	char *p = NULL, *dec;
 	int j, narg, par;
 
 	term.esc &= ~(ESC_STR_END|ESC_STR);
@@ -1872,8 +1872,6 @@ strhandle(void)
 			return;
 		case 52:
 			if (narg > 2) {
-				char *dec;
-
 				dec = base64dec(strescseq.args[2]);
 				if (dec) {
 					xsetsel(dec);
